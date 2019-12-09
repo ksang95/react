bb546b58ee420ee612c04519ee5b01b4678b6850
ksang95 <ksanghe2@naver.com>
2019-12-09 16:25:41 +0900

    Updates

---
+++ precache-manifest.72148cb8e2782c709e67bf14101b6ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f6d7e44da883e54606a8807fa18536",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d37fd29d459291879474",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "893d43ff54f0b28ca083",
+    "url": "/react/static/js/2.03ac1f37.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react/static/js/2.03ac1f37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d37fd29d459291879474",
+    "url": "/react/static/js/main.f7b8d2e8.chunk.js"
+  },
+  {
+    "revision": "f49bf78effdc812512aa",
+    "url": "/react/static/js/runtime-main.f75b8315.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
